28fe8777d678c44f9ac928aae017ae5ae44d0050
MurilloLog <ga.murillogutierrez@ugto.mx>
2023-09-11 13:25:25 -0600

    Audio source

---
+++ Assets/GustavoScripts/SoundEffect.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffect : MonoBehaviour
+{
+    public AudioSource src;
+    public AudioClip sfx1;
+
+    /*public void Button1()
+    {
+        src.clip = sfx1;
+        src.Play();
+    }*/
+
+    private void Start()
+    {
+        src.clip = sfx1;
+        src.Play();
+    }
+}
